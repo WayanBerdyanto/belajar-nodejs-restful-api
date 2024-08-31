f1bc4868d36cde2eed14c26538a69c2b02432c07
Wayan Berdyanto <122479414+WayanBerdyanto@users.noreply.github.com>
2024-08-31 18:30:08 +0700

    Add user.test Login

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,7 +1,10 @@
 import {ResponseError} from "../error/response-error.js";
 
 const validation = (scema, request) => {
-  const result = scema.validate(request);
+  const result = scema.validate(request, {
+    abortEarly: false,
+    allowUnknown: false
+  });
   if (result.error) {
     throw new ResponseError(400, result.error.message);
   } else {

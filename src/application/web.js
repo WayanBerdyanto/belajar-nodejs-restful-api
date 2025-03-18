47b972ade1ac935db9cace76e2b18a65f241edab
WayanBerdyanto <aber.panak.atok@gmail.com>
2025-03-18 09:02:25 +0700

    Add Cors

--- src/application/web.js
+++ src/application/web.js
@@ -2,8 +2,14 @@ import express from 'express';
 import { publicRouter } from '../route/public-api.js';
 import { errorMiddleware } from '../middleware/error-middleware.js';
 import { userRouter } from '../route/api.js';
+import cors from 'cors'
 
 export const web = express();
+
+web.use(cors({
+    origin: 'http://localhost:5173', // Allow only this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed HTTP methods
+}));
 
 web.use(express.json());
 

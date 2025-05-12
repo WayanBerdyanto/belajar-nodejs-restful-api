59aca9c0b2126a0db2f96bd7f0828a09d3c48954
WayanBerdyanto <aber.panak.atok@gmail.com>
2025-05-12 13:35:11 +0700

    Add Cors

--- src/application/web.js
+++ src/application/web.js
@@ -7,18 +7,19 @@ import cors from 'cors'
 export const web = express();
 
 // Local
-// web.use(cors({
-//     origin: 'http://localhost:5173', // Allow only this origin
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed HTTP methods
-// }));
+web.use(cors({
+    origin: 'http://localhost:5173', // Allow only this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed HTTP methods
+}));
 
 // Product
-web.use(cors({
-    origin: process.env.VERCEL_ENV
-        ? process.env.VERCEL_URL
-        : 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
-}));
+// web.use(cors({
+//     origin: process.env.VERCEL_ENV
+//         ? process.env.VERCEL_URL
+//         : 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+// }));
+
 web.use(express.json());
 
 web.use(publicRouter);
